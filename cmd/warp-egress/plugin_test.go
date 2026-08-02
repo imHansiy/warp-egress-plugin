b1a906a166ff4c72d9c818bd0594d3cfc36765e6
hansiy <github@hansiy.net>
2026-08-02 23:08:53 +0800

    test: update expected version to 0.2.1

--- cmd/warp-egress/plugin_test.go
+++ cmd/warp-egress/plugin_test.go
@@ -220,7 +220,7 @@ func TestPanelHTMLContainsImprovedWorkflows(t *testing.T) {
 }
 
 func TestPluginVersionIsUpdated(t *testing.T) {
-	if pluginVersion != "0.2.0" {
-		t.Fatalf("pluginVersion = %q, want 0.2.0", pluginVersion)
+	if pluginVersion != "0.2.1" {
+		t.Fatalf("pluginVersion = %q, want 0.2.1", pluginVersion)
 	}
 }
